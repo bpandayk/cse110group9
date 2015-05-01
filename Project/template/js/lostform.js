bc0a85947decefe34cf659cf01236966d0803572
Bibek Panday-kshetri <bpandayk@ucsd.edu>
2015-04-30 22:30:01 -0700

    lost form updated

--- Project/template/js/lostform.js
+++ Project/template/js/lostform.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
-  $("#submit").click(function() {
-    var x= $("#phone").val();
-    //if(x == null || (x.length <0 && x.length>10))
-      alert("Invalid Phone Number" + x)
-  });
+
 });
+  $("#submit").click(function() {
+    console.log("Submit");
+   var x= $("#phone").val();
+   var y = $("#lostname").val();
+   var m = $("#item").val();
+   var n = $("#lostemail").val();
+   var o = $("#lostdate").val();
+   var p = $("#itemdesc").val();
+   
+   var headers = {
+           "X-Parse-Application-Id": "NJy4H7P2dhoagiSCTyoDCKrGbvfaTI1sGCygKTJc",
+           "X-Parse-REST-API-Key": "RHHtZvYCPb4AOiy2psXnkLlf1uyuD7RJQxUDoQ1Y"
+        };
+    var userData = {"phone": Number(x),
+    "name":String(y),
+    "item":String(m),
+    "email":String(n),
+    "loc":String(o),
+    "descp":String(p)};
+
+
+
+    var data = JSON.stringify(userData);
+
+    $.ajax({
+      "type":"POST",
+      "url":"https://api.parse.com/1/classes/Lost",
+      "data":data,
+      "contentType":"application/json",
+      "dataType":"json",
+      "headers":headers
+
+    });
+  });

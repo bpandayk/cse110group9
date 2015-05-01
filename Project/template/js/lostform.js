8aee9a81e1892248e0a892bd5d4cc18145bc3700
Bibek Panday-kshetri <bpandayk@ucsd.edu>
2015-05-01 10:19:38 -0700

    Lost form update

--- Project/template/js/lostform.js
+++ Project/template/js/lostform.js
@@ -1,15 +1,46 @@
 $(document).ready(function() {
 
 });
+
+  $(function()  {
+    $("#lostdate").datepicker();
+  });
+
+
+  $(function(){
+    //initially hide the textbox
+    $("#othername").hide();
+    $('#item').change(function() {
+      if($(this).find('option:selected').val() == "Other"){
+        $("#othername").show();
+      }else{
+        $("#othername").hide();
+      }
+    });
+    $("#othername").keyup(function(ev){
+      var othersOption = $('#item').find('option:selected');
+      if(othersOption.val() == "Other"){
+        ev.preventDefault();
+        //change the selected drop down text
+       $(othersOption).html($("#othername").val());
+       }
+    });
+  });
+
+
   $("#submit").click(function() {
     console.log("Submit");
    var x= $("#phone").val();
    var y = $("#lostname").val();
    var m = $("#item").val();
+   if(m == "Other")
+     m = $("#othername").val();
+
+
    var n = $("#lostemail").val();
    var o = $("#lostdate").val();
    var p = $("#itemdesc").val();
-   
+
    var headers = {
            "X-Parse-Application-Id": "NJy4H7P2dhoagiSCTyoDCKrGbvfaTI1sGCygKTJc",
            "X-Parse-REST-API-Key": "RHHtZvYCPb4AOiy2psXnkLlf1uyuD7RJQxUDoQ1Y"

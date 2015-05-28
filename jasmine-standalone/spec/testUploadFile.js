9f0c6f1b56b5dd9db5e6e8bee9aee2491989d4c1
liu tan <peachtan0924@gmail.com>
2015-05-28 10:09:57 -0700

    git rid of warnings, but testUploadFile not shown on the test page

--- jasmine-standalone/spec/testUploadFile.js
+++ jasmine-standalone/spec/testUploadFile.js
@@ -1,15 +1,20 @@
 $( document ).ready(function() {
-   function File(name) {this.name = name; this.url = "something";}
-describe("test UploaderFile ", function() {
-  var file = new File("/home/uan4ik/Desktop/index.jpeg");
-  var objID = "D5YiwzHhbL";
-  var testUploader = new ParseUploader();
-    testUploader.uploadFile(file, objID, "Lost");
-  it("Uploader file function works fine", function() {
-    testUploader.linkDataTo = jasmine.createSpy("uploadFile spy called successfully!");
-//    var spyEvent = spyOn(testUploader, 'linkDataTo');
-    expect(testUploader.linkDataTo).toHaveBeenCalled();
-  });
-  });
+    function File(name) {this.name = name; this.url = "something";}
+    describe("test UploaderFile ", function() {
+        var file = new File("/home/uan4ik/Desktop/index.jpeg");
+        var objID = "D5YiwzHhbL";
+        var testUploader = new ParseUploader();
+        it("hey", function() {});
+        beforeEach(function () {
+            ParseUploader.prototype.linkDataTo = jasmine.createSpy("uploadFile spy called successfully!");
+            //testUploader.linkDataTo = jasmine.createSpy("uploadFile spy called successfully!");
+            testUploader.uploadFile(file, objID, "Lost");
+            //    var spyEvent = spyOn(testUploader, 'linkDataTo');
+        });
+        it("Uploader file function works fine", function() {
+            //expect(testUploader.linkDataTo).toHaveBeenCalled();
+            expect(ParseUploader.prototype.linkDataTo).toHaveBeenCalled();
+        });
+    });
 });
 

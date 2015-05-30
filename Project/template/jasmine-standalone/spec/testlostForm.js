f8701e03591c8144dff2365b725c0ba5101bc5ff
Uan Sholanbayev <uan4ik@gmail.com>
2015-05-30 16:21:34 -0700

    added uploadFile test file to jasmine

--- Project/template/jasmine-standalone/spec/testlostForm.js
+++ Project/template/jasmine-standalone/spec/testlostForm.js
@@ -1,6 +1,27 @@
 
+$.getScript("../../js/Uploader.js", function(){
+
+   //alert("Script loaded and executed.");
+
+   // Use anything defined in the loaded script...
+});
+/*
+this.currItem = new ItemSpec(
+        $("#lostname"),
+        ($('#item').val() === "Other") ?
+            $("#othername") : $("#item"),
+        $("#lostemail"),
+        $("#lostdate"),
+        $("#phone"),
+        $("#lastloc"),
+        $("#img"),
+        $("#itemdesc")
+    );
+*/
+//var uploader = new ParseUploader();
 describe("LostForm functionality test", function() {
   it("should be true anyway", function() {
-    expect(true).toEqual(true);
+//    expect(LostForm(uploader).currItem.toEqual(currItem);
+      expect(true).toEqual(true);
   });
 });

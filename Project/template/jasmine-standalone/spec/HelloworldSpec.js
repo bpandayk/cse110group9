7af3149aec578a8017fe9d055d9db44b42a7a84f
Bibek Panday-kshetri <bpandayk@ucsd.edu>
2015-06-06 16:52:06 -0700

    solved error

--- Project/template/jasmine-standalone/spec/HelloworldSpec.js
+++
@@ -1,5 +0,0 @@
-describe("Ex. 1:  Hello world", function() {
-  it("says hello", function() {
-    expect(helloWorld()).toEqual("Hello world!");
-  });
-});
--- Project/template/jasmine-standalone/spec/lostformSpec.js
+++
@@ -1,8 +0,0 @@
-//testing validate function for phone numberO
-
-
-//testing validate function for email
-
-
-
-//testing validate function for

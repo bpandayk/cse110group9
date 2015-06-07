12aaa38c6bffc69f5686594d7a45d72427e4a259
Uan Sholanbayev <usholanb@ucsd.edu>
2015-06-06 23:23:58 -0700

    created test for foundform.js

--- jasmine-standalone/spec/testFoundForm.js
+++ jasmine-standalone/spec/testFoundForm.js
@@ -1,8 +1,10 @@
 describe("Testing FoundForm", function(){
-   it("FormManager", function() {
-	spyOn(window, 'alert');
+   it("alert when uploader is not valid", function() {
+ 	spyOn(window, 'alert');
 	var my_FoundM = new FoundForm(null);
 	//expect(true).toEqual(true);
 	expect(window.alert).toHaveBeenCalledWith('FoundForm has to have an Uploader!');
+
+//      expect(true).toBe(true);
    });
 });

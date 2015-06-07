849252a079d29bb66076eb05628a32b0d3580c30
Uan Sholanbayev <usholanb@ucsd.edu>
2015-06-07 03:38:31 -0700

    need to pull

---
+++ jasmine-standalone/spec/testFormManager.js
@@ -0,0 +1,5 @@
+describe("testing FormManager.js", function(){
+  it("test whether supporting files were imported correctly",function(){
+    expect(true).toBe(true);
+  });
+});
---
+++ jasmine-standalone/spec/testlistall.js
@@ -0,0 +1,37 @@
+describe("Testing listall.js", function(){
+  it("hideout was hidden successfully", function(){
+    var hideout = $(".hideout");
+    var spyHideout = spyOn(hideout, "hide");
+    window["main"]();
+    hideout.hide();
+    expect(spyHideout).toHaveBeenCalled();
+  });
+  it("check phone validity", function(){
+    var fphone = 444;
+    var fakeValidator = new Validator();
+    var result = fakeValidator.validPhone(fphone);
+    expect(result).toEqual(false);
+  });
+  it("check email validity", function(){
+    var femail = "fake";
+    var fakeValidator = new Validator();
+    var result = fakeValidator.validEmail(femail);
+    expect(result).toEqual(false);
+  });
+  it("SMS object is NOT valid", function(){
+    var fphone = 858333;
+    var fname = "fakename";
+    var fbody = "fakesmsbody";
+    var fakeValidator = new Validator();
+    var result = fakeValidator.validSmsform(fname, fphone, fbody);
+    expect(result).toEqual(false);
+  });
+  it("SMS object is valid", function(){
+    var fphone = 8586564333;
+    var fname = "fakename";
+    var fbody = "fakesmsbody";
+    var fakeValidator = new Validator();
+    var result = fakeValidator.validSmsform(fname, fphone, fbody);
+    expect(result).toEqual(true);
+  });
+});
